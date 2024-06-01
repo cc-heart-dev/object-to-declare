923990d6a746c98e00bb6758018c692432a4596a
Carl Chen <ccheartszzz@gmail.com>
2024-06-01 16:57:32 +0800

    chore: remove dist bundle

--- dist/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { type ObjectToDtsOptions } from './helper';
-export default function generateTypeDeclaration(target: unknown, options?: ObjectToDtsOptions): string;
-export * from './version';
